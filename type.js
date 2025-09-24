6892ab243352a5e9a5390aa115d2eb63d5695f52
Wz-00 <wildanjk14@gmail.com>
2025-09-24 14:56:28 +0700

    feat: test code variable

---
+++ type.js
@@ -0,0 +1,9 @@
+// file: concat.js
+const variable1 = "MAQDIS";
+const variable2 = 'Academy';
+const variable3 = 'is';
+const variable4 = 'awesome';
+
+const sentence1 = `${variable1} ${variable2} ${variable3} ${variable4}`;
+console.log(sentence1); 
+
